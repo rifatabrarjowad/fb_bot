fa076f8e499c1a7a7f8cb39f8b5451e7261173b7
Rifat Abrar Jowad <rajdiprifat2004@gmail.com>
2021-04-07 23:09:08 +0600

    adding file

---
+++ main.py
@@ -0,0 +1,29 @@
+#pip install PyAutoGUI
+#import pyautogui
+#import time
+#while True:
+    #time.sleep(1)
+  #  pyautogui.typewrite('noman vaiya kmn achn :) ')
+  #  pyautogui.press('enter')
+
+
+
+#pip install PyAutoGUI
+import pyautogui
+import time
+while True:
+    time.sleep(4)
+    while True:
+        # Python3 code to iterate over a list
+        list = ["bou amar <3 ;-* ", " xoilja ;-* "]
+
+        # Getting length of list
+        length = len(list)
+        i = 0
+
+        # Iterating using while loop
+
+        while i < length:
+             pyautogui.typewrite(list[i])
+             pyautogui.press('enter')
+             i += 1
